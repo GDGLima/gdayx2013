00280035964aad729cc1a29385f5f874c0313746
emedinaa <emedinaa@gmail.com>
2013-10-08 14:23:16 -0500

    test particles

---
+++ Web/pruebas/js/app.js
@@ -0,0 +1,34 @@
+/*
+@author Eduardo Medina 
+*/
+
+(function() 
+{
+
+	window.requestAnimFrame = (function()
+	{
+		return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (callback) 
+		{
+			window.setTimeout(callback, 1000 / 60);
+		};
+	})();
+
+	document.addEventListener('DOMContentLoaded', init);
+ 
+ 	function createLogoGDGLima()
+ 	{
+ 		
+ 	}
+ 	
+	function init() 
+	{
+		console.log("init ");
+		createLogoGDGLima();
+	}
+
+})();
